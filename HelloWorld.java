5a5d34fa9a2d7a068a236ff50633ae50dc5e0b15
Tuukka Tilsa <35810432+khorngrim@users.noreply.github.com>
2020-01-29 00:48:27 +0200

    Create HelloWorld.java
    Ei oikeudet ihan riittäneet pushiin, niin laitetaa näin.
    -Tuukka

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Testitiedosto Luupin koodipajaa varten.
+ * Tällä voi harjoitella versionhallinnan käyttöä.
+ * 
+ */
+ 
+public class HelloWorld {
+    
+    private static final String tervehdys = "Moi koodipaja";
+    
+    // Tulostaa tervehdyksen
+    public static void main(String args[]) {
+        System.out.println(tervehdys);
+    }
+}
